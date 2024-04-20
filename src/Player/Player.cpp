69f72ef89160418e34c8c27a8c7a441eb587146f
BlackTigerKnight <144640335+BlackTigerKnight@users.noreply.github.com>
2024-04-21 05:37:42 +0900

    player update
    プレイヤーアニメーション追加

--- src/Player/Player.cpp
+++ src/Player/Player.cpp
@@ -1,11 +1,14 @@
 #include <Dxlib.h>
 #include "Player.h"
+#include "../Input/Input.h"
 
-// mainに書いたものクラス分け引っ越し中...
-// 家でクラス化作業します...
+#define ANIME_MAX 4		// アニメの最大数
+#define ANIME_TIME 30	// アニメの時間
 
 Player player[MAX_PLAYER];
 
+int animTime[MAX_PLAYER] = { 0 };	// アニメの変更時間
+int anim[MAX_PLAYER] = { 0 };		// 現在のアニメ
 
 void Player::Init() {
 
@@ -18,28 +21,45 @@ void Player::Init() {
 		player[i].m_living_flg = true;
 	}
 
+	player[0].m_turn_flg = true;
+	player[1].m_turn_flg = false;
+
+	// 画像ハンドル
+	player[0].m_hndl[0] = LoadGraph(NORMAL1);
+	player[0].m_hndl[1] = LoadGraph(MOVE1);
+	player[1].m_hndl[0] = LoadGraph(NORMAL2);
+	player[1].m_hndl[1] = LoadGraph(MOVE2);
+
 }
 
 void Player::Step() {
 
 	// 1プレイヤー操作
-	if (CheckHitKey(KEY_INPUT_D) != 0)	// 右移動
+	if (CheckHitKey(KEY_INPUT_D) != 0) {	// 右移動
 		player[0].m_speed.x += 0.5f;
-	if (CheckHitKey(KEY_INPUT_A) != 0)	// 左
+		player[0].m_turn_flg = true;
+	}
+	if (CheckHitKey(KEY_INPUT_A) != 0) {	// 左
 		player[0].m_speed.x -= 0.5f;
-	if (CheckHitKey(KEY_INPUT_W) != 0)	// 上
+		player[0].m_turn_flg = false;
+	}
+	if (CheckHitKey(KEY_INPUT_W) != 0)		// 上
 		player[0].m_speed.y -= 0.5f;
-	if (CheckHitKey(KEY_INPUT_S) != 0)	// 下
+	if (CheckHitKey(KEY_INPUT_S) != 0)		// 下
 		player[0].m_speed.y += 0.5f;
 
 	// 2プレイヤー操作
-	if (CheckHitKey(KEY_INPUT_RIGHT) != 0)	// 右移動
+	if (CheckHitKey(KEY_INPUT_RIGHT) != 0) { // 右移動
 		player[1].m_speed.x += 0.5f;
-	if (CheckHitKey(KEY_INPUT_LEFT) != 0)	// 左
+		player[1].m_turn_flg = true;
+	}
+	if (CheckHitKey(KEY_INPUT_LEFT) != 0) {	 // 左
 		player[1].m_speed.x -= 0.5f;
-	if (CheckHitKey(KEY_INPUT_UP) != 0)		// 上
+		player[1].m_turn_flg = false;
+	}
+	if (CheckHitKey(KEY_INPUT_UP) != 0)		 // 上
 		player[1].m_speed.y -= 0.5f;
-	if (CheckHitKey(KEY_INPUT_DOWN) != 0)	// 下
+	if (CheckHitKey(KEY_INPUT_DOWN) != 0)	 // 下
 		player[1].m_speed.y += 0.5f;
 
 	// プレイヤーの座標更新
@@ -84,6 +104,51 @@ void Player::Step() {
 void Player::Draw() {
 
 	if (player[0].m_living_flg == true) {
+		if (anim[0] == 0) {
+			DrawRectGraphF(player[0].m_pos.x, player[0].m_pos.y, 0, 0, 40, 40, player[0].m_hndl[0], true, player[0].m_turn_flg);
+		}
+		else if (anim[0] == 1) {
+			DrawRectGraphF(player[0].m_pos.x, player[0].m_pos.y, 0, 40, 40, 40, player[0].m_hndl[0], true, player[0].m_turn_flg);
+		}
+		else if (anim[0] == 2) {
+			DrawRectGraphF(player[0].m_pos.x, player[0].m_pos.y, 40, 0, 40, 40, player[0].m_hndl[0], true, player[0].m_turn_flg);
+		}
+		else if (anim[0] == 3) {
+			DrawRectGraphF(player[0].m_pos.x, player[0].m_pos.y, 40, 40, 40, 40, player[0].m_hndl[0], true, player[0].m_turn_flg);
+		}
+
+	}
+	if (player[1].m_living_flg == true) {
+		if (anim[1] == 0) {
+			DrawRectGraphF(player[1].m_pos.x, player[1].m_pos.y, 0, 0, 40, 40, player[1].m_hndl[0], true, player[1].m_turn_flg);
+		}
+		else if (anim[1] == 1) {
+			DrawRectGraphF(player[1].m_pos.x, player[1].m_pos.y, 0, 40, 40, 40, player[1].m_hndl[0], true, player[1].m_turn_flg);
+		}
+		else if (anim[1] == 2) {
+			DrawRectGraphF(player[1].m_pos.x, player[1].m_pos.y, 40, 0, 40, 40, player[1].m_hndl[0], true, player[1].m_turn_flg);
+		}
+		else if (anim[1] == 3) {
+			DrawRectGraphF(player[1].m_pos.x, player[1].m_pos.y, 40, 40, 40, 40, player[1].m_hndl[0], true, player[1].m_turn_flg);
+		}
+
+	}
+
+	for (int i = 0; i < MAX_PLAYER; i++) {
+		animTime[i]++;
+		if (animTime[i] == ANIME_TIME) {
+			animTime[i] = 0;
+			anim[i]++;
+			if (anim[i] == ANIME_MAX) {
+				anim[i] = 0;
+			}
+		}
+	}
+
+
+	// (仮)
+	/*
+	if (player[0].m_living_flg == true) {
 		DrawBox(player[0].m_pos.x, player[0].m_pos.y,
 			player[0].m_pos.x + 40.0f, player[0].m_pos.y + 40.0f, GetColor(0, 255, 0), true);
 	}
@@ -91,12 +156,43 @@ void Player::Draw() {
 		DrawBox(player[1].m_pos.x, player[1].m_pos.y,
 			player[1].m_pos.x + 40.0f, player[1].m_pos.y + 40.0f, GetColor(155, 0, 255), true);
 	}
+	*/
 
 }
 
+void Player::Fin() {
+	for (int i = 0; i < MAX_PLAYER; i++) {
+		for (int j = 0; j < 2; j++) {
 
-VECTOR Player::GetStatePos(int n) { return player[n].m_pos; };
+			DeleteGraph(player[i].m_hndl[j]);
+		}
+	}
+}
 
-void Player::SetStatepos(VECTOR statepos,int n) {
+
+VECTOR Player::Getpos(int n) { return player[n].m_pos; };
+
+void Player::Setpos(VECTOR pos,int n) {
+	player[n].m_pos = pos;
+}
+
+
+float Player::GetXpos(int n) { return player[n].m_pos.x; };
+
+void Player::SetXpos(float x,int n) {
+	player[n].m_pos.x = x;
+}
+
+
+float Player::GetYpos(int n) { return player[n].m_pos.y; };
+
+void Player::SetYpos(float y,int n) {
+	player[n].m_pos.y = y;
+}
+
+
+VECTOR Player::GetStatepos(int n) { return player[n].m_statepos; };
+
+void Player::SetStatepos(VECTOR statepos, int n) {
 	player[n].m_statepos = statepos;
 }
--- src/Player/Player.h
+++ src/Player/Player.h
@@ -1,24 +1,42 @@
 #pragma once
 
-#define PATH1 "data/play/normal1_40.png"
+#define NORMAL1 "data/play/normal1_40.png"
+#define NORMAL2 "data/play/normal2_40.png"
+#define MOVE1	"data/play/move1_40.png"
+#define MOVE2	"data/play/move2_40.png"
 
 const int MAX_PLAYER = 2;	// プレイヤーの最大数
+const float SPEED = 0.1f;	// プレイヤーの速度
 
 class Player {
 private:
 	VECTOR m_pos = { 0.0f,0.0f };		// 座標
 	VECTOR m_statepos = { 0.0f,0.0f };	// 初期地点(リスポーン地点座標)
 	VECTOR m_speed = { 0.0f ,0.0f };	// 移動速度
-	int m_hndl;							// 画像ハンドル
+	int m_hndl[2];						// 画像ハンドル
 	bool m_living_flg = true;			// 生存しているかフラグ(trueで生存)
+	bool m_turn_flg = false;			// プレイヤーの向き変更
+
+	int m_animTime = 0;					// アニメの変更時間
+	int m_anim = 0;						// 現在のアニメ
 
 public:
 	void Init();
 	void Step();
 	void Draw();
+	void Fin();
 
 
-	VECTOR GetStatePos(int n);						// リスポーン地点取得
+	VECTOR Getpos(int n);							// 座標取得
+	void Setpos(VECTOR pos, int n);					// 座標設定	(nは配列の要素)
+
+	float GetXpos(int n);							// X座標取得
+	void SetXpos(float x, int n);					// X座標設定
+
+	float GetYpos(int n);							// Y座標取得
+	void SetYpos(float y, int n);					// Y座標設定
+
+	VECTOR GetStatepos(int n);						// リスポーン地点取得
 	void SetStatepos(VECTOR statepos, int n);		// リスポーン地点設定
 
 };
