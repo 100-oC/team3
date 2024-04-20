88231a12f65935ae9f8c3a62a638e0831a68c77c
100-oC <mitotonngoria@outlook.jp>
2024-04-20 22:34:33 +0900

    Update Title.h

--- src/Scene/Title/Title.h
+++ src/Scene/Title/Title.h
@@ -1,8 +1,37 @@
 #pragma once
 
+enum TITLE_IMAGE
+{
+	TITLE_BACKGROUND,
+	TITLE_TITLE,
+	TITLE_MEAT1,
+	TITLE_MEAT2,
+	TITLE_PLAYER,
+	TITLE_MANUSL,
+
+	TITLE_MAX_NUM,
+};
+
+const char TITLE_IMAGE_PATH[TITLE_MAX_NUM][255]
+{
+	"data/title/background.png",
+	"data/title/title.png",
+	"data/title/meat1.png",
+	"data/title/meat2.png",
+	"data/title/player.png",
+	"data/title/manual.png",
+};
+
 class Title
 {
 private:
+
+	int hndl[TITLE_MAX_NUM];
+
+	int x[TITLE_MAX_NUM];
+	int y[TITLE_MAX_NUM];
+
+	int fade[TITLE_MAX_NUM];
 
 public:
 	//èâä˙âª
