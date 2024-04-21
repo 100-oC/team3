4de236771387c299bbf17d065dd6fc4fd25fb79c
BlackTigerKnight <144640335+BlackTigerKnight@users.noreply.github.com>
2024-04-21 20:07:21 +0900

    player update
    プレイヤー更新
    ファイル肉追加

--- src/Player/Player.h
+++ src/Player/Player.h
@@ -14,9 +14,14 @@ private:
 	VECTOR m_pos = { 0.0f,0.0f };		// 座標
 	VECTOR m_statepos = { 0.0f,0.0f };	// 初期地点(リスポーン地点座標)
 	VECTOR m_speed = { 0.0f ,0.0f };	// 移動速度
+
 	int m_hndl[2];						// 画像ハンドル
+
 	bool m_living_flg = true;			// 生存しているかフラグ(trueで生存)
 	bool m_turn_flg = false;			// プレイヤーの向き変更
+
+	int m_carry_meat = 0;				// 現在運んでいる肉の数
+	int m_goal_meat = 0;				// 現在自分のリス地に運んた肉の数
 
 	int m_animTime = 0;					// アニメの変更時間
 	int m_anim = 0;						// 現在のアニメ
@@ -39,5 +44,11 @@ public:
 
 	VECTOR GetStatepos(int n);						// リスポーン地点取得
 	void SetStatepos(VECTOR statepos, int n);		// リスポーン地点設定
+
+	int GetCarrymeat(int n);						// 現在運んでいる肉の数取得
+	void SetCarrymeat(int carrymeat, int n);		// 現在運んでいる肉の数設定
+
+	int GetGoalmeat(int n);							// 現在自分のリス地に運んた肉の数取得
+	void SetGoalmeat(int goalmeat, int n);			// 現在自分のリス地に運んた肉の数設定
 
 };
